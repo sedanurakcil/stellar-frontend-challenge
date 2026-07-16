e3fa58953755d2387361057be7d8cba9218a4464
Sedanur Akçil <sdnr.akcil@gmail.com>
2026-07-17 02:43:03 +0300

    Added new colors value

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        background: "var(--bg)",
+        foreground: "var(--text)",
+      },
       animation: {
-        'spin': 'spin 1s linear infinite',
+        spin: "spin 1s linear infinite",
       },
     },
   },
   plugins: [],
-}
+};
